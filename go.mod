d3d466438f0f1de87871df60595743a542d7bb69
congxia wu <1398173038@qq.com>
2024-03-13 15:37:24 +0800

    v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module TestTest
+
+go 1.21.1
+
+require github.com/deer7777/TestAPI v1.0.2
---
+++ test.go
@@ -0,0 +1,9 @@
+package TestTest
+
+import (
+	"github.com/deer7777/TestAPI"
+)
+
+func Test() string {
+	return TestAPI.RemoveAPI()
+}
